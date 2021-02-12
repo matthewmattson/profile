4b73000d33fdcf11dc01c358607edbbbbc10eb34
Matt Mattson <matthewmattson1@gmail.com>
2021-02-11 19:39:03 -0700

    version 1
    finished up version 1 of the website.

--- swupscript.js
+++ swupscript.js
@@ -1,6 +1,30 @@
-const swup = new Swup()
 
 
 function goBack() {
   window.history.back();
 }
+
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+ 
+  
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
